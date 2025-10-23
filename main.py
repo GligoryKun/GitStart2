fb352684ac8d3ee2824c1a1bca2b16330c867cc9
GligoryKun <guri4ni@gmail.com>
2025-10-23 16:25:51 +0200

    Nihongo tesuto

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 print('GIT if deaficult, kusooo! eigo ga wakaranai yooo!')
+print('oke, koko made wa wakarusoodesu, tsuzukeooo')
+a = input('Input your japanese language level').lower()
+if 'n1' in a:
+    print('ahaha, omae wa tsuyoi desu yo ne!?, yoi kaeri mo ii na')
+else:
+    print('looooh')
+    
