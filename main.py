938e98a37addd862e60432d80ff4f75f665d4cb4
GligoryKun <guri4ni@gmail.com>
2025-10-23 16:59:12 +0200

    right test nihongo

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ print('GIT if deaficult, kusooo! eigo ga wakaranai yooo!')
 print('oke, koko made wa wakarusoodesu, tsuzukeooo')
 a = input('Input your japanese language level').lower()
 if 'n1' in a:
-    print('ahaha, omae wa tsuyoi desu yo ne!?, yoi kaeri mo ii na')
+    print('ahaha, omae wa tsuyoi desu yo ne!?, oii, kaette mo ii zo')
 else:
     print('looooh')
     
